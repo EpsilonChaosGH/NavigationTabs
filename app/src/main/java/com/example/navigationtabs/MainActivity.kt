0b8dfb76b726a7c01c6b7e6d3c38c1e6c5865f92
Artem Morozov <darkness256i@gmail.com>
2023-01-17 20:06:14 +0300

    fix rotate bug

--- app/src/main/java/com/example/navigationtabs/MainActivity.kt
+++ app/src/main/java/com/example/navigationtabs/MainActivity.kt
@@ -2,22 +2,12 @@ package com.example.navigationtabs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-            val navHost =
-                supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment
-            val navController = navHost.navController
-
-            val graph = navController.navInflater.inflate(R.navigation.main_graph)
-            graph.setStartDestination(R.id.tabsFragment)
-            navController.graph = graph
-        }
+    }
 
 }
